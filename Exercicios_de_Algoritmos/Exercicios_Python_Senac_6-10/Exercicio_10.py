4785ae4e3d4c47797e8903da78508cff2765fbac
Jone Sayd <112839474+jonesayd@users.noreply.github.com>
2022-09-25 23:59:15 -0400

    Add resposta ao exercício

--- Exercicios_de_Algoritmos/Exercicios_Python_Senac_6-10/Exercicio_10.py
+++ Exercicios_de_Algoritmos/Exercicios_Python_Senac_6-10/Exercicio_10.py
@@ -1,1 +1,18 @@
 #10. Faça um algoritmo que leia a largura e altura de uma parede, calcule e mostre a área a ser pintada e a quantidade de tinta necessária para o serviço, sabendo que cada litro de tinta pinta uma área de 2metros quadrados.
+
+print("--------------------------------------------------")
+print("         CÁLCULO M² E QUANTIDADE DE TINTA         ")
+print("--------------------------------------------------")
+
+larg=float(input("Insira a largura (m): "))
+alt=float(input("Insira a altura (m): "))
+
+print("--------------------------------------------------")
+
+area=larg*alt
+qtd_tinta=area/2
+
+print("A área total a ser pintada é de: ", area, "m².")
+print("Será(ão) usada(os): ", qtd_tinta, "litros de tinta.")
+
+print("--------------------------------------------------")
