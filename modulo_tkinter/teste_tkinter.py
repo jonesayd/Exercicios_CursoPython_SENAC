25d3ab36a4389729f027fd0c5f317d846a32bc3b
Jone Sayd <112839474+jonesayd@users.noreply.github.com>
2022-10-06 23:30:13 -0400

    Create teste_tkinter.py

---
+++ modulo_tkinter/teste_tkinter.py
@@ -0,0 +1,8 @@
+# Teste do módulo de linguagem Python 'TKinter' para criação de interfaces gráficas:
+
+import tkinter #Comando usado para importar todo o conteúdo da biblioteca (tudo o que o Tkinter tem a oferecer)
+
+objeto_janela = tkinter.Tk() #Para criar o objeto, nesse caso a janela de entrada da nossa aplicação
+objeto_janela.title("Teste de Janela") #Método utilizado para alterar o título da janela
+objeto_janela.mainloop() #O objeto aberto fica em loop até que uma instrução permita encerrar a aplicação 
+
